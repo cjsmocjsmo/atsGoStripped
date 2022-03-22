8ac8087ef6dc9ef13c1437a0dc32e102d7f758f1
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-03-22 07:07:58 -0700

    fix err

--- CreateCert.sh
+++ CreateCert.sh
@@ -3,3 +3,9 @@
 openssl req  -new  -newkey rsa:2048  -nodes  -keyout alphatree.key  -out alphatree.csr && \
 openssl  x509  -req  -days 365  -in alphatree.csr  -signkey alphatree.key  -out alphatree.crt && \
 docker-compose up -d --build
+
+# apt-get install -y snapd && \
+# snap install core && \ 
+# snap refresh core && \
+# snap install --classic certbot && \
+# certbot certonly --standalone
